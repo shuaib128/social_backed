3aff3cb532582c2b25cf3ac967e042b16741e3e0
shuaib128 <shuaibahamed77@gmail.com>
2021-12-10 10:29:57 +0600

    removed react

--- socialsite/src/Api/BackendHost.js
+++
@@ -1,2 +0,0 @@
-// export const BackendHost = "https://shuaibsocialbackend.herokuapp.com"
-export const BackendHost = "http://localhost:8000"
--- socialsite/src/components/ServerHost.js
+++
@@ -1,1 +0,0 @@
-export const ServerHost = "http://127.0.0.1:8000"
